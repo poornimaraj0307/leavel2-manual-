18e2e942420bddb4e9c668c6941501927f668930
poornimaraj0307 <poornimaraj0307@gmail.com>
2025-07-31 14:33:39 +0530

    Add files via upload

---
+++ py10.py
@@ -0,0 +1,14 @@
+print("Exponential Program")
+print("--------------------")
+x=int(input("Enter X value:"))
+y=int(input("Enter Y value:"))
+z=int(input("Enter Z value:"))
+print("Result")
+p=(x*x)+(y*y)+(z*z)
+print("P:",p)
+q=(x*x*x)+(y*y)+z+20
+print("Q:",q)
+r=(x*x*y)+(y*y*z)+(z*z*x)+10
+print("R:",r)
+s=(x*x*x*y*y*z)+2*(x*y*z)+(x*y*z)
+print("S:",s)
