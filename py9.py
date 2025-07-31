1c927349450c53db5d46f6d0125492c0e950a2d9
poornimaraj0307 <poornimaraj0307@gmail.com>
2025-07-31 14:31:42 +0530

    Add files via upload

---
+++ py9.py
@@ -0,0 +1,10 @@
+print("Sum of N number")
+print("----------------")
+n=int(input("Enter the N value:"))
+print("Result")
+a=n*(n+1)/2
+print("Sum:",a)
+b=n*(n+1)*((2*n)+1)/6
+print("Sum Square:",b)
+c=(n*(n+1)/2)**2
+print("Sum Cube",c)
